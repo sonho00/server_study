6de1a9d633c03c73a90f377cd9ae35c28dccea6c
Laborat <sonho00@naver.com>
2026-04-14 11:01:20 +0900

    Test: Reproduce race condition in multi-thread summation

---
+++ Main/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+int g_sum = 0;
+
+void Add()
+{
+    for (int i = 0; i < 100000; i++) {
+        g_sum++;
+    }
+}
+
+int main()
+{
+    std::vector<std::thread> workers;
+
+    for (int i = 0; i < 10; i++) {
+        workers.push_back(std::thread(Add));
+    }
+
+    for (auto& t : workers) {
+        t.join();
+    }
+
+    std::cout << "Result: " << g_sum << std::endl;
+
+    return 0;
+}
