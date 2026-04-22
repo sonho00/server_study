a299549f71819a5d22fdb100b2051fc64252e99b
Laborat <sonho00@naver.com>
2026-04-22 18:14:37 +0900

    Feat: 인덱스 스왑 방식을 이용한 고성능 오브젝트 풀 구현
    기존 free indices 방식을 인덱스 배열 스왑 관리 방식으로 교체
    shared_ptr 커스텀 삭제자와 Capture/Release 로직을 연동하여 중복 반납 방지
    객체 반납 시 불필요한 데이터 이동 및 복잡한 컨테이너 탐색 로직 제거

--- Network/IOCP/ObjectPool.hpp
+++ Network/IOCP/ObjectPool.hpp
@@ -2,7 +2,7 @@
 
 #include <memory>
 #include <mutex>
-#include <stack>
+#include <numeric>
 
 template <typename T>
 class PoolElement : public std::enable_shared_from_this<T> {
@@ -10,47 +10,54 @@ class PoolElement : public std::enable_shared_from_this<T> {
 	virtual ~PoolElement() = default;
 
 	void Capture() { selfPtr_ = this->shared_from_this(); }
-	void Release() { selfPtr_ = nullptr; }
+	void Release() { selfPtr_.reset(); }
 
-	size_t poolIndex;
+	void SetPoolIdx(size_t idx) { poolIdx_ = idx; }
+	size_t GetPoolIdx() const { return poolIdx_; }
 
    private:
+	size_t poolIdx_ = 0;
 	std::shared_ptr<T> selfPtr_ = nullptr;
 };
 
-template <typename T, size_t PoolSize>
+template <typename T, size_t PoolSize = 1024>
 class ObjectPool {
    public:
-	ObjectPool() {
-		for (size_t i = 0; i < PoolSize; ++i) {
-			freeIndices_.push(i);
-		}
+	ObjectPool() { std::iota(std::begin(idx_), std::end(idx_), 0); }
+
+	std::shared_ptr<T> Acquire() {
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (count_ == PoolSize) return nullptr;
+
+		size_t objectPos = idx_[count_];
+		T* object = &pool_[objectPos];
+
+		object->SetPoolIdx(count_);
+
+		auto sptr =
+			std::shared_ptr<T>(object, [this](T* obj) { this->Release(obj); });
+		sptr->Capture();
+
+		++count_;
+		return sptr;
 	}
 
-	std::shared_ptr<T> Pop() {
+	void Release(T* object) {
 		std::lock_guard<std::mutex> lock(mutex_);
-		if (freeIndices_.empty()) {
-			return nullptr;
-		}
-		T* obj = pool_ + freeIndices_.top();
-		freeIndices_.pop();
-		std::shared_ptr<T> sharedObj(new (obj) T(),
-									 [this](T* obj) { this->Push(obj); });
-		sharedObj->Capture();
-		return sharedObj;
+		if (count_ == 0) return;
+
+		size_t currentPos = object->GetPoolIdx();
+		size_t lastObjectPos = idx_[--count_];
+		
+		std::swap(idx_[currentPos], idx_[count_]);
+		pool_[lastObjectPos].SetPoolIdx(currentPos);
+
+		object->Release();
 	}
 
    private:
-	void Push(T* obj) {
-		std::lock_guard<std::mutex> lock(mutex_);
-		size_t index = obj - pool_;
-		obj->~T();
-		if (index < PoolSize) {
-			freeIndices_.push(index);
-		}
-	}
-
 	T pool_[PoolSize];
-	std::stack<size_t> freeIndices_;
+	size_t idx_[PoolSize];
+	size_t count_ = 0;
 	std::mutex mutex_;
 };
