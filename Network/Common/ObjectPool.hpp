5839e9b9b13bd269815eb98b5a3d108653d47343
Laborat <sonho00@naver.com>
2026-04-29 18:08:27 +0900

    feat: Index와 Generation을 조합한 고유 ID 생성 로직 구현
    idx와 generation을 결합하여 중복 및 재사용 문제를 방지하는 세션 ID 체계 구축
    오브젝트 풀에서 ID 추출 및 유효성 검증을 위한 기반 마련

--- Network/Common/ObjectPool.hpp
+++ Network/Common/ObjectPool.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <memory>
 #include <mutex>
 #include <numeric>
@@ -10,8 +11,14 @@ class PoolElement : public std ::enable_shared_from_this<T> {
 		requires std::derived_from<U, PoolElement<U>>
 	friend class ObjectPool;
 
+	uint64_t GetHandle() const {
+		return (static_cast<uint64_t>(generation_) << 32) | who_;
+	}
+
    private:
 	size_t where_ = 0;
+	size_t who_ = 0;
+	size_t generation_ = 0;
 };
 
 template <typename T, size_t PoolSize = 1024>
@@ -22,34 +29,48 @@ class ObjectPool {
 		std::iota(std::begin(who_), std::end(who_), 0);
 	}
 
-	std::shared_ptr<T> Acquire() {
-		size_t newPerson;
-		T* obj = nullptr;
-		{
-			std::lock_guard<std::mutex> lock(mutex_);
-			if (activeCount_ >= PoolSize) {
-				return nullptr;
-			}
-			newPerson = who_[activeCount_];
-			obj = new (pool_ + newPerson * sizeof(T)) T();
-			obj->where_ = activeCount_;
-			++activeCount_;
-		}
-
-		return std::shared_ptr<T>(obj, [this, newPerson](T* ptr) {
-			std::lock_guard<std::mutex> lock(mutex_);
-			--activeCount_;
-			T* lastPerson =
-				reinterpret_cast<T*>(pool_ + who_[activeCount_] * sizeof(T));
-			lastPerson->where_ = ptr->where_;
-			std::swap(who_[ptr->where_], who_[activeCount_]);
-			ptr->~T();
-		});
-	}
+	std::shared_ptr<T> Acquire();
 
    private:
+	void Release(T* obj);
+
 	alignas(T) std::byte pool_[sizeof(T) * PoolSize];
 	size_t who_[PoolSize];
+	size_t generation_[PoolSize];
 	size_t activeCount_ = 0;
 	std::mutex mutex_;
 };
+
+template <typename T, size_t PoolSize>
+	requires std::derived_from<T, PoolElement<T>>
+std::shared_ptr<T> ObjectPool<T, PoolSize>::Acquire() {
+	size_t newPerson;
+	T* obj = nullptr;
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (activeCount_ >= PoolSize) {
+			return nullptr;
+		}
+		newPerson = who_[activeCount_];
+		obj = new (pool_ + newPerson * sizeof(T)) T();
+		obj->where_ = activeCount_;
+		++activeCount_;
+	}
+	obj->who_ = newPerson;
+	obj->generation_ = generation_[newPerson];
+
+	return std::shared_ptr<T>(obj, [this](T* obj) { Release(obj); });
+}
+
+template <typename T, size_t PoolSize>
+	requires std::derived_from<T, PoolElement<T>>
+void ObjectPool<T, PoolSize>::Release(T* obj) {
+	std::lock_guard<std::mutex> lock(mutex_);
+	--activeCount_;
+	T* lastPerson =
+		reinterpret_cast<T*>(pool_ + who_[activeCount_] * sizeof(T));
+	lastPerson->where_ = obj->where_;
+	std::swap(who_[obj->where_], who_[activeCount_]);
+	++generation_[obj->who_];
+	obj->~T();
+}
