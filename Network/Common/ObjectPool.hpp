fa1ffcd72980bd6752e71d7c63d43dc4cf816333
Laborat <sonho00@naver.com>
2026-04-29 17:09:04 +0900

    refactor: swap-pop 구현 및 Generation 도입
    swap-pop 알고리즘을 통한 O(1) 객체 반납 및 재사용 구조 구축

--- Network/Common/ObjectPool.hpp
+++ Network/Common/ObjectPool.hpp
@@ -3,42 +3,53 @@
 #include <memory>
 #include <mutex>
 #include <numeric>
-#include <vector>
 
 template <typename T>
-class PoolElement : public std::enable_shared_from_this<T> {
-   public:
+class PoolElement : public std ::enable_shared_from_this<T> {
+	template <typename U, size_t N>
+		requires std::derived_from<U, PoolElement<U>>
+	friend class ObjectPool;
+
    private:
+	size_t where_ = 0;
 };
 
 template <typename T, size_t PoolSize = 1024>
 	requires std::derived_from<T, PoolElement<T>>
 class ObjectPool {
    public:
-	ObjectPool() : freeList_(PoolSize) {
-		std::iota(freeList_.begin(), freeList_.end(), 0);
+	ObjectPool() : activeCount_(0) {
+		std::iota(std::begin(who_), std::end(who_), 0);
 	}
 
 	std::shared_ptr<T> Acquire() {
-		size_t index;
+		size_t newPerson;
+		T* obj = nullptr;
 		{
 			std::lock_guard<std::mutex> lock(mutex_);
-			if (freeList_.empty()) {
+			if (activeCount_ >= PoolSize) {
 				return nullptr;
 			}
-			index = freeList_.back();
-			freeList_.pop_back();
+			newPerson = who_[activeCount_];
+			obj = new (pool_ + newPerson * sizeof(T)) T();
+			obj->where_ = activeCount_;
+			++activeCount_;
 		}
-		T* obj = new (pool_ + index * sizeof(T)) T();
-		return std::shared_ptr<T>(obj, [this, index](T* ptr) {
-			ptr->~T();
+
+		return std::shared_ptr<T>(obj, [this, newPerson](T* ptr) {
 			std::lock_guard<std::mutex> lock(mutex_);
-			freeList_.push_back(index);
+			--activeCount_;
+			T* lastPerson =
+				reinterpret_cast<T*>(pool_ + who_[activeCount_] * sizeof(T));
+			lastPerson->where_ = ptr->where_;
+			std::swap(who_[ptr->where_], who_[activeCount_]);
+			ptr->~T();
 		});
 	}
 
    private:
 	alignas(T) std::byte pool_[sizeof(T) * PoolSize];
-	std::vector<size_t> freeList_;
+	size_t who_[PoolSize];
+	size_t activeCount_ = 0;
 	std::mutex mutex_;
 };
