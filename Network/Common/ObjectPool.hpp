f79cedc00fe22c6c5af352591dcde31ead17583e
Laborat <sonho00@naver.com>
2026-05-02 19:50:06 +0900

    feat: ObjectPool에 isLazy 옵션 추가
    isLazy 옵션을 통해 객체 사전 생성과 지연 생성 선택 가능
    false(기본값): 생성자에서 미리 객체 풀을 채워 런타임 성능 최적화
    true: 필요 시점에 객체를 생성하여 초기 메모리 점유 및 구동 시간 최소화

--- Network/Common/ObjectPool.hpp
+++ Network/Common/ObjectPool.hpp
@@ -3,14 +3,28 @@
 #include <array>
 #include <memory>
 
-template <typename T, size_t N>
+template <typename T, size_t N, bool isLazy = false>
 	requires std::derived_from<T, std::enable_shared_from_this<T>>
 class ObjectPool {
    public:
+	ObjectPool() {
+		if constexpr (!isLazy) {
+			for (size_t i = 0; i < N; ++i) {
+				new (&pool_[i * sizeof(T)]) T();
+			}
+		}
+	}
+
 	template <typename... Args>
 	std::shared_ptr<T> Acquire(size_t idx, Args&&... args) {
-		T* obj = new (&pool_[idx * sizeof(T)]) T(std::forward<Args>(args)...);
-		return std::shared_ptr<T>(obj, [this, idx](T* ptr) { ptr->~T(); });
+		T* obj = reinterpret_cast<T*>(&pool_[idx * sizeof(T)]);
+		if constexpr (isLazy) {
+			obj = new (obj) T(std::forward<Args>(args)...);
+		}
+
+		return std::shared_ptr<T>(obj, [this, idx](T* ptr) {
+			if constexpr (isLazy) ptr->~T();
+		});
 	};
 
    private:
